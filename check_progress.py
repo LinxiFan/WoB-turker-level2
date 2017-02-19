f4cbd491c7a64c368a41fe2c0f19a9334f1bcc82
LinxiFan <jimfanspire@gmail.com>
2017-02-18 16:50:22 -0800

    New script to replace download.sh

---
+++ check_progress.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+-s: connect to ssh
+"""
+import os
+import json
+import sys
+from urllib.request import urlretrieve
+
+AWS = 'ec2-52-33-170-48.us-west-2.compute.amazonaws.com'
+
+if len(sys.argv) > 1:
+    os.system('ssh -i wob_openai.pem ubuntu@'+AWS)
+    sys.exit(0)
+
+
+HTTP = 'http://{}:8080/data/{}'
+urlretrieve(HTTP.format(AWS, 'level2.jsonl'), filename='data/level2.jsonl')
+urlretrieve(HTTP.format(AWS, 'progress.json'), filename='data/progress.json')
+
+os.system('wc -l data/level2.jsonl')
+os.system('less data/level2.jsonl')
+progress = json.load(open('data/progress.json'))
+for code, val in progress.items():
+    if val > 0:
+        print(code, '+', val)
