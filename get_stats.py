7302f4104cfa4c58919dc0a4bfa8e6e7738bd0eb
LinxiFan <jimfanspire@gmail.com>
2017-02-24 14:01:03 -0800

    Rename all 'special' gui to 'other'

--- get_stats.py
+++ get_stats.py
@@ -6,6 +6,9 @@ n_templates = 0
 n_examples = 0
 semantics = Counter()
 example_per_template = Counter()
+n_slots = Counter()
+n_gui = Counter()
+n_guitag = 0
 has_answers = 0
 for entry in open('data/combined.jsonl'):
     n_templates += 1
@@ -14,9 +17,18 @@ for entry in open('data/combined.jsonl'):
     dat = entry['data']
     example_per_template[len(dat)] += 1 
     n_examples += len(dat)
+    if 'ANSWER' in dat[0]:
+        n_slots[len(dat[0]) - 1] += 1
+        if len(dat[0]) == 7: print(entry['question'])
+    else:
+        n_slots[len(dat[0])] += 1
+        if len(dat[0]) == 6: print(entry['question'])
     for d in dat:
         if 'ANSWER' in d:
             has_answers += 1
+    if 'gui' in entry:
+        n_gui.update(entry['gui'])
+        n_guitag += 1
 
 print('n_templates', n_templates)
 print('n_examples', n_examples)
@@ -26,4 +38,10 @@ for k, count in semantics.most_common():
     print('\t', k, '=', count)
 print('Example per template')
 for k, count in example_per_template.most_common():
+    print('\t', k, '=', count)
+print('Number of slots')
+for k, count in n_slots.most_common():
+    print('\t', k, '=', count)
+print('{} entries have GUI tags'.format(n_guitag))
+for k, count in n_gui.most_common():
     print('\t', k, '=', count)
